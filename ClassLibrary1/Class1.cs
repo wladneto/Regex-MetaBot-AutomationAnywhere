26f254c9e514bd81b71042d5063d4d20644933ab
Wladimir Teixeira Neto <wladimirtn@algartech.com>
2019-03-31 18:35:50 -0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ClassLibrary1
+{
+        public class RegexValidador
+        {
+            public static string RegexMatch(string pattern, string text)
+            {
+                Regex regex = new Regex(pattern);
+                Match m = regex.Match(text);
+                if (!m.Success)
+                {
+                    return "False";
+                }
+                else {
+                    return "True";
+                }   
+            } 
+        } 
+}
